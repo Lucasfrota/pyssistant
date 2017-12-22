11a6e38e46773d9953e8f6763782f6941b00707f
Lucasfrota <frota_lv@hotmail.com>
2017-12-21 20:24:29 -0400

    new method add_dialog

--- CODE/util/dialog.py
+++ CODE/util/dialog.py
@@ -1,10 +1,11 @@
 from listen import listen
 from get_audio import get_audio
+from util.models.feature import feature
 from sentence_analysis_module import SentenceAnalysis
 
 class Dialog:
 
-    def __init__(self, dialog_list):
+    def __init__(self, dialog_list=[]):
         self.dialog_list = dialog_list
         self.sentence_analyzer = SentenceAnalysis(self.dialog_list)
 
@@ -16,3 +17,6 @@ class Dialog:
         audio = listen()
         result = get_audio(audio)
         self.sentence_analyzer.get_main_command(result)
+
+    def add_dialog(self, command, answer="", function=None):
+        self.dialog_list.append(feature(command, answer = answer, function = None))
